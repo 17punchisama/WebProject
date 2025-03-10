e3e2d0c28dafca6fa88fba9b2aa771e7a4e80356
Taparov <guitar2990@gmail.com>
2025-03-11 00:44:06 +0700

    Search

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -25,7 +25,56 @@ namespace WebProject.Controllers
                                 .ToListAsync();
             return View(posts);
         }
+        public async Task<IActionResult> Search(string input)
+        {
+            var result = await _context.Tags
+                .Where(t => t.Name == input)
+                .Include(t => t.PostTags)
+                    .ThenInclude(pt => pt.Post) // Include related Post
+                    .ThenInclude(p => p.Owner) // Include related Owner in Post
+                 .Select(t => new
+                {
+                    TagPosts = t.PostTags.Select(pt => pt.Post).ToList(), // Navigate through PostTag
+                })
+                .FirstOrDefaultAsync();
 
+            if (result != null)
+            {
+                foreach (var post in result.TagPosts)
+                {
+                    if (post != null)
+                    {
+                        await _context.Entry(post)
+                        .Collection(p => p.ParticipantPosts)
+                        .LoadAsync();
+
+                        await _context.Entry(post)
+                        .Collection(p => p.PostTags)
+                        .LoadAsync();
+
+                        foreach (var pt in post.PostTags)
+                        {
+                            await _context.Entry(pt)
+                            .Reference(p => p.Tag)
+                            .LoadAsync();
+                        }
+                    } 
+                }
+            }
+
+            // If no matching tag is found, still get MatchingPosts separately
+            List<Post> matchingPosts = await _context.Posts
+                .Where(p => p.Title.Contains(input))
+                .Include(p => p.Owner)
+                .Include(p => p.ParticipantPosts)
+                .Include (p => p.PostTags)
+                    .ThenInclude(pp => pp.Tag)
+                .ToListAsync();
+
+            List<Post> tagPosts = result?.TagPosts ?? new List<Post>();
+            ViewBag.InputString = input;
+            return View((matchingPosts, tagPosts));
+        }
         public IActionResult Privacy()
         {
             return View();
